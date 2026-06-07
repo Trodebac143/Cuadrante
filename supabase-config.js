b6474b68d1727d68d456bc96f3e8c67cc3aed27c
Vic <viflyse@gmail.com>
2026-06-07 19:08:02 +0200

    Add files via upload
    Inicio del repositorio

---
+++ supabase-config.js
@@ -0,0 +1,7 @@
+// Configuración Supabase
+// 1) Crea el proyecto gratuito en Supabase.
+// 2) Ejecuta el archivo supabase_setup.sql en el SQL Editor.
+// 3) Copia aquí Project URL y anon public key.
+
+window.UPO4_SUPABASE_URL = "https://bdakopfmrnbxsakwrund.supabase.co";
+window.UPO4_SUPABASE_ANON_KEY = "sb_publishable_MYvpV9Mo6VPESbTD7bLwbw__L10ZwgV";
